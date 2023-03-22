573b28433cbad06d37b397a50aa33391474509ea
Drew Bowling <andrewwbowling@gmail.com>
2023-03-22 12:43:50 -0600

    finish test 4

--- src/book.js
+++ src/book.js
@@ -1,14 +1,20 @@
 function createTitle(title) {
     
-  
   return `The ${title}`
 }
 
-
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return mainCharacter
+}
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
