1403e3f348358edeb9e6718bb047ad1876425285
Drew Bowling <andrewwbowling@gmail.com>
2023-03-22 19:28:40 -0600

    passed 4 tests in shelf...SLEEEP

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,12 @@
 
-function shelfBook() {
-
+function shelfBook(book, shelf) {  
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
 }
 
+//add books (objects) to a specific shelf [array]. 
+// the variables are global push to add them to the array.
 
 module.exports = {
    shelfBook,
